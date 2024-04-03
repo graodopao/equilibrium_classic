89c147e4543b7aa73439bf4b7d563ef225b44aa1
Tiago Costa Araujo <37983882+graodopao@users.noreply.github.com>
2024-04-03 16:31:24 -0300

    Repository files restructuring

---
+++ AprendendoSDLUau/GameEntity.h
@@ -0,0 +1,53 @@
+#ifndef _GAMEENTITY_H
+#define _GAMEENTITY_H
+#include "MathHelper.h"
+
+class GameEntity
+{
+public:
+
+	enum SPACE 
+	{
+		local = 0, 
+		world = 1
+	};
+
+	float mRotation;
+
+private:
+
+	Vector2 mPos;
+	Vector2 mScale;
+
+	bool mActive;
+	GameEntity* mParent;
+
+public:
+
+	GameEntity(float x = 0.0f, float y = 0.0f);
+	~GameEntity();
+
+	void Pos(Vector2 pos);
+	Vector2 Pos(SPACE space = world);
+
+	void Rotation(float rotation);
+	float Rotation(SPACE space = world);
+
+	void Scale(Vector2 scale);
+	Vector2 Scale(SPACE space = world);
+
+	void Active(bool active);
+	bool Active();
+
+	void Parent(GameEntity* parent);
+	GameEntity* Parent();
+
+	void Translate(Vector2 vec);
+	void Rotate(float amount);
+
+	virtual void Update();
+	virtual void Render();
+};
+
+
+#endif
